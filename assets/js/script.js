49b1c6fcb53e026bed9fb2ea784ea5a96b4ff788
dimstar <kylermagee@gmail.com>
2018-03-02 21:13:36 -0800

    Added html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 /** Script for tivial trivia game */
+
+var answers = {
+    'one': 'Solid State Drive',
+    'two': 'The Legend of Zelda',
+    'three': 'Bitcoin',
+    'four': 'Universal Serial Bus',
+    'five': 'Liquid Crystal Display',
+    'six': 'Read-only Memory'
+}
+
+$("#imDone").on('click', function(){
+
+});
